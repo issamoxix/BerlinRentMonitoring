3b5e085c6d8c841ff76714f41774c17086406717
issamoxix <issamhaidaoui@gmail.com>
2023-04-07 15:32:46 +0200

    code refactor

--- helpers/helper.py
+++ helpers/helper.py
@@ -14,8 +14,8 @@ def send_email(offer, provider, temp=0):
     :return: (int) 1 if the email was sent successfully, 0 otherwise.
     """
     current_time = datetime.now().strftime("%H:%M:%S")
-    secrets_file = os.path.join(os.getcwd(), 'secrets.json')
-    with open(secrets_file, 'r') as f:
+    secrets_file = secrets_checker()
+    with open(secrets_file, "r") as f:
         secrets = json.load(f)
     try:
         recipient_email = secrets["reciepient"]["email"]
@@ -24,9 +24,9 @@ def send_email(offer, provider, temp=0):
         subject = provider
         body = json.dumps(offer)
         message = f"Subject: {subject}\n\n{body}"
-        
+
         smtp_server = secrets["server"]["smptp_server"]
-        smtp_port = 587
+        smtp_port = secrets["server"]["port"]
         smtp_username = secrets["sender"]["email"]
         smtp_password = secrets["sender"]["password"]
         smtp_connection = smtplib.SMTP(smtp_server, smtp_port)
@@ -40,7 +40,31 @@ def send_email(offer, provider, temp=0):
         return 1
     except Exception as e:
         if temp == 0:
-            return send_email(offer["href"], f"[NEW OFFER] ({current_time}) ", temp=temp + 1)
-        print(f'[Email] Body  {json.dumps(offer)}')
+            return send_email(
+                offer["href"], f"[NEW OFFER] ({current_time}) ", temp=temp + 1
+            )
+        print(f"[Email] Body  {json.dumps(offer)}")
         print(f"[Email] not sent due to an error: {e}")
         return 0
+
+
+def secrets_checker():
+    """
+    Check if secrets.json file exists. If not, create one with default values.
+    Return the path to the secrets file.
+    """
+    secrets_path = os.path.join(os.getcwd(), "secrets.json")
+    if os.path.exists(secrets_path):
+        return secrets_path
+
+    secrets = {
+        "sender": {"email": "sender@example.com", "password": "senderpassword"},
+        "recipient": {"email": "recipient@example.com"},
+        "server": {"smtp_server": "smtp.example.com", "port": 587},
+    }
+    with open(secrets_path, "w") as secret_file:
+        json.dump(secrets, secret_file, indent=4)
+
+    raise Exception(
+        f"A new secrets file has been created at {secrets_path}. You must set the credentials in the file."
+    )
