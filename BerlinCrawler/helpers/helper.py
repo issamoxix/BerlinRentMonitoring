c31bc777b26c96d87e81b8c5bbf2c81754c365f0
issamoxix <issamhaidaoui@gmail.com>
2023-04-07 15:36:06 +0200

    no message

---
+++ BerlinCrawler/helpers/helper.py
@@ -0,0 +1,46 @@
+import smtplib
+import json
+import os
+from datetime import datetime
+
+
+def send_email(offer, provider, temp=0):
+    """
+    Send an email with the given offer and provider information.
+
+    :param offer: (dict) Offer information to be sent in the email body.
+    :param provider: (str) Provider name to be used as the email subject.
+    :param temp: (int) Number of retries attempted if the first attempt fails.
+    :return: (int) 1 if the email was sent successfully, 0 otherwise.
+    """
+    current_time = datetime.now().strftime("%H:%M:%S")
+    secrets_file = os.path.join(os.getcwd(), 'secrets.json')
+    with open(secrets_file, 'r') as f:
+        secrets = json.load(f)
+    try:
+        recipient_email = secrets["reciepient"]["email"]
+        sender_email = secrets["sender"]["email"]
+
+        subject = provider
+        body = json.dumps(offer)
+        message = f"Subject: {subject}\n\n{body}"
+        
+        smtp_server = secrets["server"]["smptp_server"]
+        smtp_port = 587
+        smtp_username = secrets["sender"]["email"]
+        smtp_password = secrets["sender"]["password"]
+        smtp_connection = smtplib.SMTP(smtp_server, smtp_port)
+        smtp_connection.starttls()
+        smtp_connection.login(smtp_username, smtp_password)
+
+        smtp_connection.sendmail(sender_email, recipient_email, message)
+        smtp_connection.quit()
+
+        print("[Email] sent ! ")
+        return 1
+    except Exception as e:
+        if temp == 0:
+            return send_email(offer["href"], f"[NEW OFFER] ({current_time}) ", temp=temp + 1)
+        print(f'[Email] Body  {json.dumps(offer)}')
+        print(f"[Email] not sent due to an error: {e}")
+        return 0
---
+++ BerlinCrawler/utils/EbayOffers.py
@@ -0,0 +1,86 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+from helpers.helper import *
+import time
+from datetime import datetime
+
+
+def EbayCrawler():
+        url = "https://www.ebay-kleinanzeigen.de/s-wohnung-mieten/berlin/anzeige:angebote/c203l3331+wohnung_mieten.swap_s:nein"
+        headers = {
+            "authority": "www.ebay-kleinanzeigen.de",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "max-age=0",
+            "referer": "https://www.ebay-kleinanzeigen.de/s-wohnung-mieten/berlin/c203l3331+wohnung_mieten.swap_s:nein",
+            "sec-ch-ua": '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+        }
+
+        count = 0
+        prev = {}
+
+        kill = True
+        email_sent = []
+        while kill:
+            try:
+                time.sleep(5)
+                response = requests.get(url, timeout=15, headers=headers)
+                soup = BeautifulSoup(response.text, "html.parser")
+                items = soup.find("ul", {"id": "srchrslt-adtable"}).find_all(
+                    "li", {"class": ["lazyload-item", "ad-listitem"]}
+                )
+                item = [
+                    i
+                    for i in items
+                    if "is-topad" not in i.get("class") and "lazyload-item" in i.get("class")
+                ][0]
+                title = item.h2.text.strip()
+                a_href = item.h2.a.get("href")
+                price = item.find(
+                    "p", {"class": "aditem-main--middle--price-shipping--price"}
+                ).text.strip()
+                try:
+                    meta_data = [
+                        i.text
+                        for i in item.find("p", {"class": "text-module-end"}).find_all("span")
+                    ]
+                except:
+                    meta_data = []
+                offer = {
+                    "title": title,
+                    "price": price,
+                    "meta": meta_data,
+                    "href": f"https://www.ebay-kleinanzeigen.de{a_href}",
+                }
+                current_time = datetime.now().strftime("%H:%M:%S")
+                if count > 0:
+                    if offer["href"] != prev["href"]:
+                        if offer['href'] in email_sent:
+                            prev = offer
+                            count += 1
+                            print("[Ebay] ",current_time, json.dumps(offer, ensure_ascii=False))
+                            continue
+                        print(
+                            "[Ebay][NEW]",
+                            current_time,
+                            json.dumps(offer, ensure_ascii=False),
+                        )
+                        send_email(offer, f"[Ebay] {offer['title']}")
+                        email_sent.append(offer["href"])
+                prev = offer
+                count += 1
+                print("[Ebay] ",current_time, json.dumps(offer, ensure_ascii=False))
+            except Exception as e:
+                print(f'[ERROR] something went wrong {e}')
+
+if __name__ == '__main__':
+    EbayCrawler()
