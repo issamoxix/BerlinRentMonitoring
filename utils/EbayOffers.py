b6c1215f0bce754e4b9533c3c2e98c977a098fc3
issamoxix <issamhaidaoui@gmail.com>
2023-04-07 20:56:44 +0200

    removed unnecessary headers

--- utils/EbayOffers.py
+++ utils/EbayOffers.py
@@ -14,14 +14,6 @@ def EbayCrawler():
             "accept-language": "en-US,en;q=0.9",
             "cache-control": "max-age=0",
             "referer": "https://www.ebay-kleinanzeigen.de/s-wohnung-mieten/berlin/c203l3331+wohnung_mieten.swap_s:nein",
-            "sec-ch-ua": '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "same-origin",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
         }
 
